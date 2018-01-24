ca2605c8a62d865e345ae80320e354839c82f7f9
Heleneb62 <helenebelvalpro@gmail.com>
2018-01-24 16:34:19 +0100

    maj

--- Exo8/footer.php
+++ Exo8/footer.php
@@ -5,6 +5,13 @@
       </nav>
       <a href="pages/mentions.php" target="_blank" class="mention">Mentions légales</a>
       <div class="nav3">
+        <a href="https://fr-fr.facebook.com/" target="_blank"><img src="img/02_facedebook.png" class="icones" alt="facedebouk"></a>
+        <a href="https://fr.linkedin.com/" target="_blank"><img src="img/07_linkekedin.png" class="icones" alt="linkekedin"></a>
+        <a href="https://www.instagram.com/?hl=fr" target="_blank"><img src="img/10_instatagram.png" class="icones" alt="instatagram"></a>
+        <a href="https://fr.wordpress.org/" target="_blank"><img src="img/11_wordpress.png" class="icones" alt="wordpress"></a>
+        <a href="https://www.pinterest.fr/" target="_blank"><img src="img/bouh.png" class="icones" alt="bouhpint"></a>
+        <a href="https://www.tumblr.com/" target="_blank"><img src="img/babou.png" class="icones" alt="babou"></a>
+        <a href="https://twitter.com/?lang=fr" target="_blank"><img src="img/paf.png" class="icones" alt="twittos"></a>
         <a href="https://plus.google.com/discover?hl=fr" target="_blank"><img src="img/14_google+.png" class="icones" alt="google+"></a>
       </div>
     </footer>
